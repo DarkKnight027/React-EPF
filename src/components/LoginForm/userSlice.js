f76c07684e994e630494fe6fc81ef2c4e1582f3b
Siddesh N <siddeshsiddu049@gmail.com>
2025-06-14 11:13:15 +0530

    HeaderUpdate

--- src/components/LoginForm/userSlice.js
+++ src/components/LoginForm/userSlice.js
@@ -1,21 +1,38 @@
+// userSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
+// Load from localStorage
+const storedUser = JSON.parse(localStorage.getItem("user")) || {};
+
 const initialState = {
-  uan: '123',
-  password: '123',
-  username: 'King',
-  isLoggedIn: false,
+  uan: storedUser.uan || '123',
+  password: storedUser.password || '123',
+  username: storedUser.username || 'King',
+  isLoggedIn: storedUser.isLoggedIn || false,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    login: (state) => {
+    login: (state, action) => {
+      const { uan, username, password } = action.payload;
       state.isLoggedIn = true;
+      state.uan = uan;
+      state.username = username;
+      state.password = password;
+
+      // Save to localStorage
+      localStorage.setItem("user", JSON.stringify({
+        uan,
+        username,
+        password,
+        isLoggedIn: true
+      }));
     },
     logout: (state) => {
       state.isLoggedIn = false;
+     
     },
     updatePassword: (state, action) => {
       state.password = action.payload;
